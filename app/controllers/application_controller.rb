4a602529e52c171472e5a5bd24808ee31f2956da
Scott Li <scottli0101@gmail.com>
2014-08-29 14:49:25 -0400

    json error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  respond_to :json
+
+  rescue_from Exception do |e|
+    uncaught_error(e)
+  end
+
+  def uncaught_error(e)
+
+   error_info = {
+     :error => "Uncaught Error",
+     :exception => "#{e.class.name} : #{e.message}",
+   }
+   error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
+
+   render :json => error_info.to_json, :status => 500
+  end
 end
---
+++ bin/local.sh
@@ -0,0 +1,1 @@
+psql postgres://auth@localhost:5432/authy_development
